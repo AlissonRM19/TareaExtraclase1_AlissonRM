800cc0051aa9b2e6e2f4fba2630fb8d7b2d41a10
Grevaa <greivincaro@estudiantec.cr>
2022-09-08 21:46:21 -0600

    Creacion de los diferentes archivos.txt

--- main.cpp
+++ main.cpp
@@ -6,14 +6,12 @@
 #include "page.h"
 
 
-
-
-
 // crea o lee el archivo txt y crea el contenido aleatorio
 int createfile(int sizefile) {
     std::string suma = std::to_string(sizefile);
-    std::ofstream newtxt ("New"+suma+"Kb.txt");
-    for (int i=0;i<sizefile;i++)
+    std::ofstream newtxt ("New"+suma+"KB.txt");
+    int numint = sizefile*256;
+    for (int i=0;i<numint;i++)
         newtxt<<rand()<<","<<std::endl;
     newtxt.close();
 }
@@ -35,40 +33,18 @@ int leer(std::string filename){
 //Array para la eleccion de tamaño de pagina
 int inpS[6]={1,4,8,12,24,36};
 
-static int multipfiles(int inpS){
-    for(int i = 0; i< sizeof(inpS);i++ ){
-        createfile(i*256);
+int multipfiles(int sizearray[]) {
+    for (int i = 0; i < 6; i++) {
+        int size = sizearray[i];
+        createfile(size);
 
     }
-
-
-
 }
-/*
-//convierte un .txt a .bin ERROR
-int converttobin(){
-    char buffer;
-    //std::string line;
-    std::ifstream myfile("New.txt");
-    std::ofstream binfile("binfile.bin",std::ios::out|std::ios::binary);
-    int nums[3];
-        while(!myfile.eof()){
-            myfile>> nums[0]>> nums[1]>> nums[2];
-            binfile.write(reinterpret_cast<const char*>(nums), 3*sizeof (int ));
-        }
-        myfile.close();
-}
-*/
-
-
-
-
-
-
 
 //Funcion principal
 int main() {
-    int multipfiles();
+    std::cout << sizeof(inpS);
+    multipfiles(inpS);
     //  leer();
 
 
