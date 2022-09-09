bd77e54e4501f8861b63dbe49d40e9594eb146a4
AlissonRM19 <alisson.r06@estudiantec.cr>
2022-09-08 18:48:52 -0600

    Prueba

--- main.cpp
+++ main.cpp
@@ -5,12 +5,7 @@
 //Funcion principal
 int main(int argc,char* argv[]) {
 
-    //std:: cout<<argv[1]<<std::endl;
-    //page **pages;
-    /*void page::ImprimirInfo()
-    {
-        cout<<page1
-    }*/
+
 
     return 0;
 }
